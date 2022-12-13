3cb8ccb1b167de638a2b14d7a2b37b6714031d14
youko509 <saintcharleserico@gmail.com>
2022-12-13 17:13:33 -0500

    add more option in thhis function

--- exo5.py
+++ exo5.py
@@ -1,10 +1,17 @@
 import re
 
-text="er3453 e-56-67-r"
+text="er3453 e-56-67-r()"
 
 def sluglify(text):
-    
-    table="abcdefghijklmnopkrstuvwxyz1234567890-"
     text=text.replace(' ','-')
-    print(text)
+    table="[^abcdefghijklmnopkrstuvwxyz1234567890-]"
+    regex = re.findall(table,text)
+    print(regex)
+    if len(regex) == len(text):
+        print(f"your slug is: {text}")
+    else:
+        for val in regex:
+            text = text.replace(val,'-')
+        print(f"unused format for slug will be replace by - ")    
+        print(text)
 sluglify(text)    
